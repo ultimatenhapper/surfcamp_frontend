e12fad26b40385216529d248f6311825a6c63d8f
Javi <belikemike3057@hotmail.com>
2024-04-05 13:43:11 +0200

    Header component. Navbar, button and styles

---
+++ app/_components/Header.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  const navItems = [
+    {
+      display: "the camp",
+      slug: "",
+    },
+    {
+      display: "the experience",
+      slug: "experience",
+    },
+    {
+      display: "the blog",
+      slug: "blog",
+    },
+  ];
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        src="/assets/logo.svg"
+        alt="logo"
+      />
+      <ul className="header__nav">
+        {navItems.map((navItem) => (
+          <li key={navItem.display}>
+            <Link href={`/${navItem.slug}`}>
+              <h5>{navItem.display}</h5>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Link href="/events">
+        <button className="btn btn--black btn--small">BOOK NOW</button>
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
